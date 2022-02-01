455de093190851c87b7b49cbaca5195d524965d5
Nuttadon <nuttadon_p@cmu.ac.th>
2022-02-01 16:30:04 +0700

    lab21_1 complete

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect a,Rect b){
+double x1=a.x,y1=a.y,x2=a.x+a.w,y2=a.y-a.h;
+double i1=b.x,j1=b.y,i2=b.x+b.w,j2=b.y-b.h;
+double area;
+double width = min(x2,i2) - max(x1,i1);
+double heigh = min(y1,j1) - max(y2,j2);
+	if(x1<i2&&y1>j2&&x2>i1&&y2<j1) {
+		area = width*heigh;
+	}
+	else area=0;
+return area;
 }
